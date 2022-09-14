b9dab170b52e4ca9ce8d6e9345d3e612d4f252cf
Vincent Sjögren <vincentsjogren@gmail.com>
2022-09-14 08:35:14 +0200

    added eslint, updated function descriptions, added isSymbol, isMap and isSet, deleted isTruthy and isFalsy

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,63 @@
-export type ValidatorCallback = (x: any) => boolean;
-export type Validator = (...args: any[]) => ReturnType<ValidatorCallback>;
+export type ValidatorCallback = (x?: unknown) => boolean;
 
 /** Create typeChecker function */
-export const createTypeChecker = (callback: ValidatorCallback): Validator => {
-	return (...args: any[]) =>
-		args.length === 0
-			? false
-			: args.length === 1
-			? callback(args[0])
-			: args.every((x) => callback(x));
+export const createTypeChecker = (callback: ValidatorCallback) => {
+	return (...args: unknown[]) =>
+		args.length < 2 ? callback(args[0]) : args.every((x) => callback(x));
 };
-/** Check if value(s) are of type 'number' and not NaN */
+/** Check if typeof value(s) is 'number' and value(s) are not NaN */
 export const isNum = createTypeChecker(
-	(x) => typeof x === 'number' && !Number.isNaN(x)
+	(x?: unknown) => typeof x === 'number' && !Number.isNaN(x)
 );
-/** Check if value(s) are of type 'number',
- * smaller than Infinity, larger than -Infinity and not NaN
+/** Check if typeof value(s) is 'number', value(s) are not NaN,
+ * value(s) are not -Infinite or Infinite
  */
-export const isFinite = createTypeChecker(
-	(x) =>
-		typeof x === 'number' &&
-		x < Infinity &&
-		x > -Infinity &&
-		!Number.isNaN(x)
+export const isFiniteNum = createTypeChecker(
+	(x?: unknown) => typeof x === 'number' && !Number.isNaN(x) && isFinite(x)
 );
-/** Check if value(s) are of type 'number', not NaN and integer value(s) */
+/** Check if typeof value(s) is 'number', value(s) are not
+ * NaN, value(s) are finite number(s) and integer value(s)
+ */
 export const isInt = createTypeChecker(
-	(x) => typeof x === 'number' && !Number.isNaN(x) && x % 1 === 0
+	(x?: unknown) =>
+		typeof x === 'number' && !Number.isNaN(x) && isFinite(x) && x % 1 === 0
 );
-/** Check if value(s) are of type 'number', not NaN and float value(s) */
+/** Check if typeof value(s) is 'number', value(s) are not
+ * NaN, value(s) are finite numbers and float value(s)
+ */
 export const isFloat = createTypeChecker(
-	(x) => typeof x === 'number' && !Number.isNaN(x) && x % 1 !== 0
+	(x?: unknown) =>
+		typeof x === 'number' && !Number.isNaN(x) && isFinite(x) && x % 1 !== 0
 );
-/** Check if value(s) are of type 'string' */
-export const isStr = createTypeChecker((x) => typeof x === 'string');
-/** Check if value(s) are of type 'boolean' */
-export const isBool = createTypeChecker((x) => typeof x === 'boolean');
-/** Check if value(s) are of type 'object' and instance(s) of Object export constructor */
+/** Check if typeof value(s) is 'string' */
+export const isStr = createTypeChecker((x?: unknown) => typeof x === 'string');
+/** Check if typeof value(s) is 'boolean' */
+export const isBool = createTypeChecker(
+	(x?: unknown) => typeof x === 'boolean'
+);
+/** Check if value(s) are not null and typeof value(s) is 'object' */
 export const isObj = createTypeChecker(
-	(x) => typeof x === 'object' && x instanceof Object
+	(x?: unknown) => x !== null && typeof x === 'object'
 );
 /** Check if value(s) are array(s), (uses Array.isArray) */
 export const isArr = createTypeChecker(Array.isArray);
-/** Check if value(s) are of type 'function' */
-export const isFn = createTypeChecker((x) => typeof x === 'function');
-/** Check if value(s) are equal to null */
-export const isNull = createTypeChecker((x) => x === null);
+/** Check if typeof value(s) is 'function' */
+export const isFn = createTypeChecker((x?: unknown) => typeof x === 'function');
+/** Check if value(s) are null */
+export const isNull = createTypeChecker((x?: unknown) => x === null);
 /** Check if value(s) are undefined */
-export const isUndef = createTypeChecker((x) => typeof x === 'undefined');
+export const isUndef = createTypeChecker((x?: unknown) => x === undefined);
 /** Check if value(s) are Date objects */
-export const isDate = createTypeChecker((x) => x instanceof Date);
+export const isDate = createTypeChecker((x?: unknown) => x instanceof Date);
 /** Check if value(s) are valid Date objects */
 export const isValidDate = createTypeChecker(
-	(x) => x instanceof Date && !isNaN(Number(x))
+	(x?: unknown) => x instanceof Date && !Number.isNaN(x.valueOf())
 );
-/** Check if value(s) are "truthy" */
-export const isTruthy = createTypeChecker((x) => !!x);
-/** Check if value(s) are "falsy" */
-export const isFalsy = createTypeChecker((x) => !x);
+/** Check if typeof value(s) is 'symbol' */
+export const isSymbol = createTypeChecker(
+	(x?: unknown) => typeof x === 'symbol'
+);
+/** Check if value(s) are Map objects */
+export const isMap = createTypeChecker((x?: unknown) => x instanceof Map);
+/** Check if value(s) are Set objects */
+export const isSet = createTypeChecker((x?: unknown) => x instanceof Set);
