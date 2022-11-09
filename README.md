5e06071182027f2a60f4efde3ac0d53b0cf18ce0
Vincent Sjögren <vincentsjogren@gmail.com>
2022-11-09 15:06:57 +0100

    removed functions with rest parameters, simplified return types with "is" keyword, moved callback functions to index.ts and removed package entrypoint "x-is-type/callbacks"

--- README.md
+++ README.md
@@ -22,123 +22,105 @@ const { isNum, isStr, isObj } = require('x-is-type');
 This package includes the following functions for type checking of one or more variables.
 
 ```js
-/** Check if typeof all values is 'number' and not NaN */
+/** Check if typeof value is 'number' and not NaN */
 isNum(1);
 // => true
-isNum(1, NaN);
+isNum(NaN);
 // => false
 
 /**
- * Check if typeof all values is 'number',
+ * Check if typeof value is 'number',
  * not NaN and integers
  */
 isInt(1);
 // => true
-isInt(1, 1.1);
+isInt(1.1);
 // => false
 
 /**
- * Check if typeof all values is 'number',
+ * Check if typeof value is 'number',
  * not NaN and floating point numbers
  */
 isFloat(1.1);
 // => true
-isFloat(1.1, 1);
+isFloat(1);
 // => false
 
-/** Check if typeof all values is 'string' */
-isStr('Hello');
+/** Check if typeof value is 'string' */
+isStr('1');
 // => true
-isStr("Hello", 1);
+isStr(1);
 // => false
 
-/** Check if typeof all values is 'boolean' */
+/** Check if typeof value is 'boolean' */
 isBool(false);
 // => true
-isBool(false, null);
+isBool(null);
 // => false
 
 /**
- * Check if typeof all values is 'object',
+ * Check if typeof value is 'object',
  * not null and instances of ObjectConstructor
  */
 isObj({ foo: 'bar' });
 // => true
-isObj({ foo: 'bar' }, null);
+isObj(null);
 // => false
 
-/** Check if all values are arrays, (uses Array.isArray) */
+/** Check if value is an array, (uses Array.isArray) */
 isArr([1, 2, 3]);
 // => true
-isArr([1, 2, 3], {});
+isArr({});
 // => false
 
-/** Check if typeof all values is 'function' */
+/** Check if typeof value is 'function' */
 isFn(() => {});
 // => true
-isFn(() => {}, []);
+isFn({});
 // => false
 
-/** Check if all values are null */
+/** Check if value is null */
 isNull(null);
 // => true
-isNull(null, undefined);
+isNull(undefined);
 // => false
 
-/** Check if all values are undefined */
+/** Check if value is undefined */
 isUndef(undefined);
 // => true
-isUndef(undefined, null);
+isUndef(null);
 // => false
 
-/** Check if all values are undefined or null */
-isNullish(undefined, null);
-// => true
-isNullish(undefined, null, 0);
-// => false
-
-/** Check if all values are instances of DateConstructor */
+/** Check if value is an instance of DateConstructor */
 isDate(new Date());
 // => true
-isDate(new Date(), Date.now());
+isDate(Date.now());
 // => false
 
 /** 
- * Check if all values are instances of DateConstructor
+ * Check if value is an instance of DateConstructor
  * and contains valid dates
  */
  isValidDate(new Date(1970, 1, 1))
  // => true
- isValidDate(new Date(1970, 1, 1), new Date("hello"))
+ isValidDate(new Date("hello"))
  // => false
  
- /** Check if typeof all values is 'symbol' */
+ /** Check if typeof value is 'symbol' */
 isSymbol(Symbol(123));
 // => true
-isSymbol(Symbol(123), 123);
+isSymbol(123);
 // => false
 
-/** Check if all values are instances of MapConstructor */
+/** Check if value is an instance of MapConstructor */
 isMap(new Map());
 // => true
-isMap(new Map(), {foo: "bar"});
+isMap({foo: "bar"});
 // => false
 
-/** Check if all values are instances of SetConstructor */
+/** Check if value is an instance of SetConstructor */
 isSet(new Set());
 // => true
-isSet(new Set(), []);
+isSet(new Array());
 // => false
-```
-
-All of the functions listed above are created using [`createTypeChecker`](https://github.com/vinsjo/x-is-type/blob/main/src/index.ts#L5) and a corresponding [`callback function`](https://github.com/vinsjo/x-is-type/blob/main/src/callbacks.ts)
-
-If performance or better TypeScript support in terms of return type (for supported types) is a priority over being able to check types of multiple values at once, the callbacks can be used instead:
-
-## Import Callbacks only:
-```js
-// ESM:
-import { isNum, isStr, isObj } from 'x-is-type/callbacks';
-// CommonJS:
-const { isNum, isStr, isObj } = require('x-is-type/callbacks');
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,49 @@
-import * as callbacks from './callbacks';
-export * as callbacks from './callbacks';
+/** Check if typeof x is 'number' and x is not NaN
+ * TS return type incorrect if x is NaN
+ */
+export const isNum = (x: unknown): x is number =>
+    typeof x === 'number' && !Number.isNaN(x);
+/** Check if typeof x is 'string' */
+export const isStr = (x: unknown): x is string => typeof x === 'string';
+/** Check if typeof x is 'boolean' */
+export const isBool = (x: unknown): x is boolean => typeof x === 'boolean';
+/** Check if typeof x is 'object', x is not null and x is instanceof Object */
+export const isObj = (
+    x: unknown
+): x is { [k: string | number | symbol]: unknown } =>
+    x !== null && typeof x === 'object' && x instanceof Object;
+/** Check if x is an array (uses Array.isArray) */
+export const isArr = (x: unknown): x is unknown[] => Array.isArray(x);
+/** Check if typeof x is 'function' */
+export const isFn = (x: unknown): x is (...args: unknown[]) => unknown =>
+    typeof x === 'function';
+/** Check if x is null */
+export const isNull = (x: unknown): x is null => x === null;
+/** Check if x is undefined */
+export const isUndef = (x: unknown): x is undefined => typeof x === 'undefined';
+/** Check if x is instanceof DateConstructor */
+export const isDate = (x: unknown): x is Date => x instanceof Date;
+/** Check if typeof x is 'symbol' */
+export const isSymbol = (x: unknown): x is symbol => typeof x === 'symbol';
+/** Check if x is instanceof MapConstructor */
+export const isMap = (x: unknown): x is Map<unknown, unknown> =>
+    x instanceof Map;
+/** Check if x is instanceof SetConstructor */
+export const isSet = (x: unknown): x is Set<unknown> => x instanceof Set;
 
-/** Create typeChecker function */
-export const createTypeChecker = <T = unknown>(
-	callback: callbacks.Validator<T>
-) => {
-	return (...values: unknown[]) =>
-		values.length < 2
-			? callback(values[0])
-			: values.every((x) => callback(x));
-};
-/** Check if typeof all values is 'number' and not NaN */
-export const isNum = createTypeChecker(callbacks.isNum);
+// TS UNCERTAIN RETURN TYPES
 /**
- * Check if typeof all values is 'number',
- * not NaN and integers
+ * Check if typeof x is 'number', x is not
+ * NaN and x is an integer
  */
-export const isInt = createTypeChecker(callbacks.isInt);
+export const isInt = (x: unknown): x is number =>
+    typeof x === 'number' && x % 1 === 0;
 /**
- * Check if typeof all values is 'number',
- * not NaN and floating point numbers
+ * Check if typeof x is 'number', x is not
+ * NaN and x is a float value
  */
-export const isFloat = createTypeChecker(callbacks.isFloat);
-/** Check if typeof all values is 'string' */
-export const isStr = createTypeChecker(callbacks.isStr);
-/** Check if typeof all values is 'boolean' */
-export const isBool = createTypeChecker(callbacks.isBool);
-/**
- * Check if typeof all values is 'object',
- * not null and instances of ObjectConstructor
- */
-export const isObj = createTypeChecker(callbacks.isObj);
-/** Check if all values are arrays, (uses Array.isArray) */
-export const isArr = createTypeChecker(callbacks.isArr);
-/** Check if typeof all values is 'function' */
-export const isFn = createTypeChecker(callbacks.isFn);
-/** Check if all values are null */
-export const isNull = createTypeChecker(callbacks.isNull);
-/** Check if all values are undefined */
-export const isUndef = createTypeChecker(callbacks.isUndef);
-/** Check if all values are undefined or null */
-export const isNullish = createTypeChecker(callbacks.isNullish);
-/** Check if all values are instances of DateConstructor */
-export const isDate = createTypeChecker(callbacks.isDate);
-/**
- * Check if all values are instances of DateConstructor
- * and contains valid dates
- */
-export const isValidDate = createTypeChecker(callbacks.isValidDate);
-/** Check if typeof all values is 'symbol' */
-export const isSymbol = createTypeChecker(callbacks.isSymbol);
-/** Check if all values are instances of MapConstructor */
-export const isMap = createTypeChecker(callbacks.isMap);
-/** Check if all values are instances of SetConstructor */
-export const isSet = createTypeChecker(callbacks.isSet);
+export const isFloat = (x: unknown): x is number =>
+    typeof x === 'number' && !Number.isNaN(x % 1) && x % 1 !== 0;
+/** Check if x is instanceof Date and contains a valid date */
+export const isValidDate = (x: unknown): x is Date =>
+    x instanceof Date && !Number.isNaN(x.getTime());
